64ef36ca0c9f04f2936413ceed9e9fcdb5151476
Ten YoDun <291746925@qq.com>
2021-02-20 13:20:27 +0800

    Site updated: 2021-02-20 13:20:20

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?33fff4d05148912adfeca11fbc8c0840";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
